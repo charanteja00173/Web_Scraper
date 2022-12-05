3ff3c49fc966e14356d231a998673375f4f31c57
Samuel Michael <97008491+charanteja00173@users.noreply.github.com>
2022-12-06 04:44:30 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -3,5 +3,5 @@ import requests
 site='site_link'
 details=requests.get(site)
 content=details.text # get details in text format
-savein=BeautifulSoup(content,'html')
-print(savein)
+savein=BeautifulSoup(content,'html') #to save in html format 
+print(savein.prettify())
